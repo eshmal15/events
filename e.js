b22b34ac743da303d459151096a594ecbcdc9b53
eshmal15 <eshmalmalik4036@gmail.com>
2024-03-23 01:44:39 +0500

    event
    today task

---
+++ e.js
@@ -0,0 +1,18 @@
+function update(){
+    console.log("event is fired");
+}
+// i want to create variable
+var x=10;
+var y=20;
+console.log(x*y);
+// I want to create variable
+ 
+ 
+ function sum(a,b){
+    return a+b;
+    //if a wanto assign a functoin to variable 
+    var mysum=sum;
+    console.log(mysum)
+ }
+ 
+ 
